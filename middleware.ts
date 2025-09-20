bdcac4ced771d97903a60e6757a43519b01c9f75
Rabbi Hossain <kdrabbihossain056@gmail.com>
2025-09-20 13:36:04 +0600

    Updated project after edits

--- middleware.ts
+++ middleware.ts
@@ -2,4 +2,10 @@ export { auth as middleware } from '@/lib/auth';
 
 export const config = {
   matcher: ['/profile'],
+ runtime: "nodejs",
 };
+// // middleware.ts
+// export const config = {
+//   matcher: ["/dashboard/:path*"],
+//   runtime: "nodejs", // 👈 Node.js runtime
+// };
